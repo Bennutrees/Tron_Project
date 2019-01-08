c7ef106d96cc665fee3424b1e0a9ae7e9d121ce1
Bennutrees <benjamin.poulet42@hotmail.fr>
2019-01-08 19:01:14 +0100

    Suppression doublon

--- tron_clientWithServer/www/js/scores.js
+++
@@ -1,7 +0,0 @@
-function change_index(name) {
-    document.getElementById('index_' + previous_index).className = 'non_active index';
-    document.getElementById('index_' + name).className = 'active index';
-    document.getElementById('content_' + previous_index).style.display = 'none';
-    document.getElementById('content_' + name).style.display = 'flex';
-    previous_index = name;
-}
